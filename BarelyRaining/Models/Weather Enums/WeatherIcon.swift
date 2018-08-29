083c843d8f132a3d7b56105640155c22aabd9c21
Nick Cracchiolo <nickcracchiolo@nicks-macbook-pro.local>
2018-08-29 10:49:42 -0700

    Added enums to convert DarkSky icon and severity fields

---
+++ BarelyRaining/Models/Weather Enums/WeatherIcon.swift
@@ -0,0 +1,78 @@
+//
+//  WeatherIcon.swift
+//  CMTWeather
+//
+//  Created by Nick Cracchiolo on 8/27/18.
+//  Copyright © 2018 Nick Cracchiolo. All rights reserved.
+//
+
+import UIKit
+
+/// Enum to parse the DarkSky "icon" string field into a usable UIImage
+enum WeatherIcon {
+    case clearDay
+    case clearNight
+    case rain
+    case snow
+    case sleet
+    case wind
+    case fog
+    case cloudy
+    case partyCloudyDay
+    case partyCloudyNight
+    case other
+    
+    init(withName name:String) {
+        switch name {
+        case "clear-day":
+            self = .clearDay
+        case "clear-night":
+            self = .clearNight
+        case "rain":
+            self = .rain
+        case "snow":
+            self = .snow
+        case "sleet":
+            self = .sleet
+        case "wind":
+            self = .wind
+        case "fog":
+            self = .fog
+        case "cloudy":
+            self = .cloudy
+        case "partly-cloudy-day":
+            self = .partyCloudyDay
+        case "partly-cloudy-night":
+            self = .partyCloudyNight
+        default:
+            self = .other
+        }
+    }
+    
+    func image() -> UIImage? {
+        switch self {
+        case .clearDay:
+            return UIImage(named: "clear-day")
+        case .clearNight:
+            return UIImage(named: "clear-night")
+        case .rain:
+            return UIImage(named: "rain")
+        case .snow:
+            return UIImage(named: "snow")
+        case .sleet:
+            return UIImage(named: "sleet")
+        case .wind:
+            return UIImage(named: "wind")
+        case .fog:
+            return UIImage(named: "fog")
+        case .cloudy:
+            return UIImage(named: "cloudy")
+        case .partyCloudyDay:
+            return UIImage(named: "partly-cloudy-day")
+        case .partyCloudyNight:
+            return UIImage(named: "partly-cloudy-night")
+        case .other:
+            return UIImage(named: "other")
+        }
+    }
+}
---
+++ BarelyRaining/Models/Weather Enums/WeatherSeverity.swift
@@ -0,0 +1,54 @@
+//
+//  WeatherSeverity.swift
+//  CMTWeather
+//
+//  Created by Nick Cracchiolo on 8/27/18.
+//  Copyright © 2018 Nick Cracchiolo. All rights reserved.
+//
+
+import Foundation
+
+/// Enum describing the three values a WeatherAlert can use for the severity of the weather
+enum WeatherSeverity {
+    /// An individual should be aware of potentially severe weather
+    case advisory
+    /// An individual should prepare for potentially severe weather
+    case watch
+    /// An individual should take immediate action to protect themselves and others from potentially severe weather
+    case warning
+    
+    init?(withName name:String) {
+        switch name {
+        case "advisory":
+            self = .advisory
+        case "watch":
+            self = .watch
+        case "warning":
+            self = .warning
+        default:
+            return nil
+        }
+    }
+    
+    func name() -> String {
+        switch self {
+        case .advisory:
+            return "Advisory"
+        case .watch:
+            return "Watch"
+        case .warning:
+            return "Warning"
+        }
+    }
+    
+    func description() -> String {
+        switch self {
+        case .advisory:
+            return "Be aware of potentially severe weather."
+        case .watch:
+            return "Prepare for potentially severe weather."
+        case .warning:
+            return "Take immediate action to protect yourself and others from potentially severe weather."
+        }
+    }
+}
