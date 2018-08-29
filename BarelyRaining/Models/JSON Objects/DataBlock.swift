193494701ccb40d33e6c89e0f62ebd3f1ba57101
Nick Cracchiolo <nickcracchiolo@nicks-macbook-pro.local>
2018-08-29 10:49:11 -0700

    Added more codable structs to parse JSON

---
+++ BarelyRaining/Models/JSON Objects/DataBlock.swift
@@ -0,0 +1,18 @@
+//
+//  DailyWeather.swift
+//  CMTWeather
+//
+//  Created by Nick Cracchiolo on 8/27/18.
+//  Copyright © 2018 Nick Cracchiolo. All rights reserved.
+//
+
+import Foundation
+
+struct DataBlock:Codable {
+    /// A human-readable summary of this data block.
+    let summary:String
+    /// A machine-readable text summary of this data block. (May take on the same values as the iconproperty of DataPoints.)
+    let icon:String?
+    /// An array of DataPoint objects, ordered by time, which together describe the weather conditions at the requested location over time.
+    let data:[DataPoint]
+}
---
+++ BarelyRaining/Models/JSON Objects/Forecast.swift
@@ -0,0 +1,31 @@
+//
+//  Forecast.swift
+//  CMTWeather
+//
+//  Created by Nick Cracchiolo on 8/27/18.
+//  Copyright © 2018 Nick Cracchiolo. All rights reserved.
+//
+
+import Foundation
+
+/// The main forecast object requested from the DarkSky API for the specified latitude and longitude.
+struct Forecast:Codable {
+    /// The requested latitude.
+    let latitude:Float
+    /// The requested longitude.
+    let longitude:Float
+    /// The IANA timezone name for the requested location. This is used for text summaries and for determining when hourly and daily DataBlock objects begin. E.G "America/New_York"
+    let timezone:String
+    /// A DataPoint object containing the current weather conditions at the requested location.
+    let currently:DataPoint?
+    /// A DataBlock object containing the weather conditions minute-by-minute for the next hour.
+    let minutely:DataBlock?
+    /// A DataBlock object containing the weather conditions hour-by-hour for the next two days.
+    let hourly:DataBlock?
+    /// A DataBlock object containing the weather conditions day-by-day for the next week.
+    let daily:DataBlock?
+    /// An array of WeatherAlert objects, which, if present, contains any severe weather alerts pertinent to the requested location.
+    let alerts:[WeatherAlert]?
+    /// A FlagObject containing miscellaneous metadata about the request.
+    let flags:FlagObject?
+}
